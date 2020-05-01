c12d21ac0fada88965e834ed9e8cb283607f6df2
Koji Machida(Ejiri) <k.ejiri@gmail.com>
2020-05-01 22:03:16 +0900

    Fabボタン追加

--- src/views/components/Foodstuff/FoodstuffList.tsx
+++ src/views/components/Foodstuff/FoodstuffList.tsx
@@ -1,6 +1,32 @@
 import React from 'react';
-import { List, ListItem, ListItemText } from '@material-ui/core';
+import {
+  List,
+  ListItem,
+  ListItemText,
+  Fab,
+  makeStyles,
+  Theme,
+  createStyles,
+} from '@material-ui/core';
+import AddIcon from '@material-ui/icons/Add';
+import { Link } from 'react-router-dom';
 import { Foodstuff } from '../../../domain/models/foodstuff';
+import { addFoodstuffScreenPath } from '../../../routePaths';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      position: 'relative',
+      height: '100%',
+      overflowY: 'scroll',
+    },
+    fabButton: {
+      position: 'absolute',
+      bottom: theme.spacing(3),
+      right: theme.spacing(3),
+    },
+  })
+);
 
 type FoodstuffListProps = {
   foodstuffs: Foodstuff[];
@@ -10,14 +36,31 @@ const FoodstuffList: React.FC<FoodstuffListProps> = (
   props: FoodstuffListProps
 ) => {
   const { foodstuffs } = props;
+  const classes = useStyles();
+
   return (
-    <List>
-      {foodstuffs.map((foodstuff) => (
-        <ListItem>
-          <ListItemText primary={foodstuff.name} />
-        </ListItem>
-      ))}
-    </List>
+    <div className={classes.root}>
+      <List>
+        {foodstuffs.map((foodstuff) => (
+          <ListItem>
+            <ListItemText primary={foodstuff.name} />
+          </ListItem>
+        ))}
+      </List>
+      <Link
+        to={{
+          pathname: addFoodstuffScreenPath,
+        }}
+      >
+        <Fab
+          color="primary"
+          aria-label="add foodstuff"
+          className={classes.fabButton}
+        >
+          <AddIcon />
+        </Fab>
+      </Link>
+    </div>
   );
 };
 

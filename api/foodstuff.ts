efa9400fbedbced1cc7d0af7754598b72e4bc907
Koji Machida(Ejiri) <k.ejiri@gmail.com>
2020-05-16 23:13:05 +0900

    関数を少し修正した

--- api/foodstuff.ts
+++ api/foodstuff.ts
@@ -2,5 +2,5 @@ import { NowRequest, NowResponse } from '@now/node';
 
 export default (request: NowRequest, response: NowResponse): void => {
   const { name = 'World' } = request.query;
-  response.status(200).send(`Hello ${name}!`);
+  response.status(200).send(`Hello ${name}!うんこーーーーーーーーー`);
 };

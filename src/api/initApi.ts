c81ebefd9fe7544454c2337047909831244b8f3a
Koji Machida(Ejiri) <k.ejiri@gmail.com>
2020-05-30 23:59:33 +0900

    少し修正

--- src/api/initApi.ts
+++ src/api/initApi.ts
@@ -1,5 +1,8 @@
+import firebase from 'firebase';
 import diConfig from '../diConfig';
+import firebaseConfig from '../firebaseConfig';
 
 export default function initApi(): void {
+  firebase.initializeApp(firebaseConfig);
   diConfig();
 }
